3cbf1e08317c2235838a937ab5cce7001eb65f6c
cade0705 <thecadeinator@gmail.com>
2017-10-29 17:46:39 -0800

    Added some buttons

--- src/WindowManager.java
+++ src/WindowManager.java
@@ -10,10 +10,27 @@ import java.awt.Font;
 import javax.swing.JMenuBar;
 import javax.swing.JLabel;
 import java.awt.Toolkit;
+import javax.swing.SwingConstants;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import java.awt.FlowLayout;
+import javax.swing.JToggleButton;
+import javax.swing.JPasswordField;
+import javax.swing.JTree;
+import java.awt.GridLayout;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.BoxLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class WindowManager extends JFrame {
 
 	private JPanel contentPane;
+	private JButton creditsButton;
+	private JButton startButton;
+	private JLabel titleLabel;
 
 	/**
 	 * Launch the application.
@@ -51,15 +68,46 @@ public class WindowManager extends JFrame {
 	public void initComponents()
 	{
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 800, 500);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 		
-		JLabel lblTheGame = new JLabel("The Game");
-		lblTheGame.setFont(new Font("Comic Sans MS", Font.PLAIN, 17));
-		contentPane.add(lblTheGame, BorderLayout.NORTH);
+		titleLabel = new JLabel("The Game");
+		titleLabel.setFont(new Font("Comic Sans MS", Font.PLAIN, 50));
+		titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		startButton = new JButton("Start");
+		startButton.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		
+		creditsButton = new JButton("Credits");
+		
+		creditsButton.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(179)
+					.addComponent(titleLabel, GroupLayout.PREFERRED_SIZE, 427, Short.MAX_VALUE)
+					.addGap(168))
+				.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+					.addContainerGap(281, Short.MAX_VALUE)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING, false)
+						.addComponent(startButton, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(creditsButton, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 221, Short.MAX_VALUE))
+					.addGap(272))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addComponent(titleLabel)
+					.addGap(50)
+					.addComponent(startButton, GroupLayout.PREFERRED_SIZE, 52, GroupLayout.PREFERRED_SIZE)
+					.addGap(40)
+					.addComponent(creditsButton, GroupLayout.PREFERRED_SIZE, 55, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(183, Short.MAX_VALUE))
+		);
+		contentPane.setLayout(gl_contentPane);
 		
 		setTitle("The Game");
 	}
@@ -69,8 +117,14 @@ public class WindowManager extends JFrame {
 	//////////////////////////////////////////////////////////
 	public void createEvents()
 	{
-		
-	}
-	
-
+		creditsButton.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent e) 
+			{
+				creditsButton.setVisible(false);
+				startButton.setVisible(false);
+				titleLabel.setVisible(false);
+			}
+		});
+	} //end createEvents()
 }
