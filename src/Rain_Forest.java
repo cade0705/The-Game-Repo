d3ffe9531ef942fe7451ed140f15e783ccab0527
cade0705 <thecadeinator@gmail.com>
2018-06-10 10:30:58 -0800

    forest path started

--- src/Rain_Forest.java
+++ src/Rain_Forest.java
@@ -9,9 +9,56 @@ public class Rain_Forest
 	   public static int counter;
 	   public static Player thePlayer = Intro.thePlayer;
 	   
-	public static void start()
+	public static void start() throws InterruptedException
 	{
-		System.out.println("check");
+		userInput = new Scanner(System.in);
+		text = "You are traveling through the dangerous rainforests of Australia";
+		charText = text.toCharArray(); print(charText); delay(700);
+		text = "While you were on your phone, you lose track of your guide and try to find your way back to civilization";
+		charText = text.toCharArray(); print(charText); delay(700);
+		text = "You come to a fork in the road after a few minutes of walking";
+		charText = text.toCharArray(); print(charText); delay(700);
+		
+		boolean validChoice = false;
+		
+		while(validChoice == false)
+		{
+			text = "Which direction will you go? North, South, East, or West?";
+			charText = text.toCharArray(); print(charText); delay(700);
+			String firstChoice = (userInput.nextLine().toLowerCase());
+			
+			//FC North
+			if(firstChoice.equals("north") || firstChoice.equals("n"))
+			{
+				text = "You chose North!";
+				charText = text.toCharArray(); print(charText); delay(700);
+				validChoice = true;
+			}
+			//FC South
+			else if(firstChoice.equals("south") || firstChoice.equals("s"))
+			{
+				System.out.println("SOUTH");
+				validChoice = true;
+			}
+			//FC East
+			else if(firstChoice.equals("east") || firstChoice.equals("e"))
+			{
+				System.out.println("EAST");
+				validChoice = true;
+
+			}
+			// FC West
+			else if(firstChoice.toLowerCase().equals("west") || firstChoice.toLowerCase().equals("w"))
+			{
+				System.out.println("WEST");
+				validChoice = true;
+			}
+			else
+			{
+				text = "That's not a correct choice";
+				charText = text.toCharArray(); print(charText); delay(700);
+			}
+		}
 	}
 	
 	public static void print(char[] chars) throws InterruptedException
